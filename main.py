e7448f795231dbfe93fda96be5be985b7c4e25b6
Roman Gersaimov <romagrizly@gmail.com>
2021-08-10 03:58:31 +0500

    add main to start all processes

---
+++ main.py
@@ -0,0 +1,6 @@
+if __name__ == "__main__":
+    from from_site_to_archives import *
+    from time import sleep
+    sleep(3)
+    from from_archives_to_result_pdfs import *
+    print("COMPLETE ALL !!!")
