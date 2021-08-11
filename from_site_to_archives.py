e31a29f426fa2eebb8dc9065ac965e69ad57e716
GerasimovRM <romagrizly@gmail.com>
2021-08-12 02:29:14 +0500

    v2: searcher update

--- from_site_to_archives.py
+++ from_site_to_archives.py
@@ -1,76 +1,101 @@
-from selenium import webdriver
+from typing import Optional, Union, List
+from configparser import ConfigParser
+import os
+import time
+
+from selenium.webdriver import FirefoxProfile, Firefox
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.keys import Keys
 
-from settings import LOGIN, PASSWORD, BASE_URL, ZIP_DIR
-import time
+
+BASE_URL = "https://priem.pstu.ru"
 
 
-profile = webdriver.FirefoxProfile()
-profile.set_preference("browser.download.folderList", 2)
-profile.set_preference("browser.download.manager.showWhenStarting", False)
-profile.set_preference("browser.download.dir", ZIP_DIR)
-profile.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/zip")
+class FromCiteToArchive:
+    def __init__(self,
+                 config_path,
+                 searcher_profile: Optional[FirefoxProfile] = None,
+                 basic_wait_time: Optional[int] = None):
 
-driver = webdriver.Firefox(firefox_profile=profile)
-# wait = WebDriverWait(driver, 10)
+        config = ConfigParser()
+        config.read(config_path)
+        login = config.get("main", "LOGIN")
+        password = config.get("main", "PASSWORD")
 
-# Login page
-driver.get(f"{BASE_URL}/user/sign-in/login")
-login_input = driver.find_element_by_id("loginform-identity")
-login_input.send_keys(LOGIN)
-password_input = driver.find_element_by_id("loginform-password")
-password_input.send_keys(PASSWORD)
-password_input.send_keys(Keys.RETURN)
-time.sleep(5)  # wait page load
+        zip_dir = os.getcwd() + "\\" + config.get("main", "ZIP_DIR")
+        if not os.path.isdir(zip_dir):
+            os.mkdir(zip_dir)
 
-# Find people page
-driver.refresh()
-with open("students.txt", encoding="utf-8") as students_file:
-    students_fio = list(map(str.strip, students_file.readlines()))
+        if not searcher_profile:
+            searcher_profile = FirefoxProfile()
+            searcher_profile.set_preference("browser.download.folderList", 2)
+            searcher_profile.set_preference("browser.download.manager.showWhenStarting", False)
+            searcher_profile.set_preference("browser.download.dir", zip_dir)
+            searcher_profile.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/zip")
 
-for student_fio in students_fio:
-    student_fio_short = (student_fio.split()[0] + " " +
-                         "".join(list(map(lambda s: s[0] + ".", student_fio.split()[1:])))).lower()
+        if not basic_wait_time:
+            basic_wait_time = 10
 
-    driver.get(f"{BASE_URL}/sandbox/all")
+        self.searcher = Firefox()
+        self.downloader = Firefox(firefox_profile=searcher_profile)
+        self.wait_searcher = WebDriverWait(self.searcher, basic_wait_time)
+        self.wait_downloader = WebDriverWait(self.searcher, basic_wait_time)
 
-    filters_open_button = driver.find_element_by_xpath(u"//a[@data-toggle='collapse']")
-    filters_open_button.click()
+        self.login = login
+        self.password = password
+        self.is_auth = False
 
-    fio_input = driver.find_element_by_id("applicationsearch-fio")
-    fio_input.clear()
-    fio_input.send_keys(student_fio)
+    def auth(self) -> bool:
+        if self.is_auth:
+            return True
+        self.searcher.get(f"{BASE_URL}/user/sign-in/login")
+        login_input = self.searcher.find_element_by_id("loginform-identity")
+        login_input.send_keys(self.login)
+        password_input = self.searcher.find_element_by_id("loginform-password")
+        password_input.send_keys(self.password)
+        password_input.send_keys(Keys.RETURN)
 
-    filter_button = driver.find_element_by_xpath(u"//button[@class='btn btn-primary']")
-    filter_button.click()
-    time.sleep(1)
+        self.downloader.get(f"{BASE_URL}/user/sign-in/login")
+        login_input = self.downloader.find_element_by_id("loginform-identity")
+        login_input.send_keys(self.login)
+        password_input = self.downloader.find_element_by_id("loginform-password")
+        password_input.send_keys(self.password)
+        password_input.send_keys(Keys.RETURN)
+        # auth validation
+        try:
+            self.wait_searcher.until(lambda driver: driver.find_element_by_id("applicationsearch-fio"))
+            self.wait_downloader.until(lambda driver: driver.find_element_by_id("applicationsearch-fio"))
+        except TimeoutException:
+            raise ValueError("Неверный логин или пароль")
+        self.is_auth = True
 
-    table = driver.find_element_by_tag_name("table")
-    table_body = table.find_element_by_tag_name("tbody")
-    table_trs = table_body.find_elements_by_tag_name("tr")
-    student_count = 0
-    for table_tr in table_trs:
-        table_tds = table_tr.find_elements_by_tag_name("td")
-        t = table_tds[3].text.lower()
-        if table_tds[3].text.lower() != student_fio_short:
-            continue
-        student_count += 1
-        table_tds[2].find_element_by_tag_name("a").click()
-        time.sleep(2)
+    def find_person_and_load_data(self, student_fio: Union[str, List[str]]):
+        if isinstance(student_fio, list):
+            student_fio = " ".join(student_fio)
+        if not self.is_auth:
+            self.auth()
 
-        driver.find_element_by_xpath("//a[@class='btn btn-info']").click()
-    if student_count == 0:
-        print(f"{student_fio} not found!")
-    elif student_count == 1:
-        print(f"{student_fio} ok!")
-    else:
-        print(f"{student_fio} duplicated!")
+        self.searcher.execute_script("location.reload(true);")
 
-
-
-
-
+        self.searcher.get(f"https://priem.pstu.ru/sandbox/all?ApplicationSearch[statusBlock]=&ApplicationSearch[fio]={student_fio}&ApplicationSearch[usermail]=&ApplicationSearch[guid]=&ApplicationSearch[campaign_code]=&ApplicationSearch[citizenship]=&ApplicationSearch[hasIndividualAchievement]=&ApplicationSearch[targetReception]=&ApplicationSearch[preferences]=&ApplicationSearch[specialityName]=&ApplicationSearch[educationForm]=&ApplicationSearch[status]=&ApplicationSearch[sent_at]=&ApplicationSearch[to_sent_at]=&ApplicationSearch[created_at]=&ApplicationSearch[to_created_at]=&ApplicationSearch[last_management_at]=&ApplicationSearch[to_last_management_at]=&ApplicationSearch[lastManagerName]=&ApplicationSearch[historyChanges]=")
+        is_empty = False
+        try:
+            WebDriverWait(self.searcher, 1).until(lambda driver: driver.find_element_by_class_name("empty"))
+            is_empty = True
+        except TimeoutException:
+            table_body = self.searcher.find_element_by_tag_name("tbody")
+            table_trs = table_body.find_elements_by_tag_name("tr")
+            student_ids = list(map(lambda t: t.get_attribute("data-key"), table_trs))
+            for student_id in student_ids:
+                self.downloader.get(f"{BASE_URL}/sandbox/view?id={student_id}")
+                self.downloader.find_element_by_xpath("//a[@class='btn btn-info']").click()
+        if is_empty:
+            print(f"{student_fio} не найден!")
+        else:
+            print(f"{student_fio} ok!")
+    
+    def close(self):
+        self.searcher.close()
+        time.sleep(10)
+        self.close()
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-if __name__ == "__main__":
-    from from_site_to_archives import *
-    from time import sleep
-    sleep(3)
-    from from_archives_to_result_pdfs import *
-    print("COMPLETE ALL !!!")
+from from_site_to_archives import FromCiteToArchive
+
+
+parser = FromCiteToArchive("config.ini")
+
+with open("students.txt", encoding="utf-8") as students_file:
+    for student_fio in map(str.strip, students_file.readlines()):
+        parser.find_person_and_load_data(student_fio)
+parser.close()
