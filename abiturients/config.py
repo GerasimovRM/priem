ca27d85baf3d06b5b401669a051cc13794e3d328
Roman Gersaimov <romagrizly@gmail.com>
2022-07-29 01:43:28 +0500

    abiturients checker

---
+++ abiturients/config.py
@@ -0,0 +1,17 @@
+import os
+
+from dotenv import load_dotenv
+
+
+def debug_prestring(st: str) -> str:
+    if DEBUG:
+        return "DEBUG_" + st
+    return st
+
+
+load_dotenv()
+
+DEBUG = True if os.getenv("DEBUG") in ("True", "true", "1") else False
+
+MONGODB_URL = os.getenv(debug_prestring("MONGODB_URL"))
+PARSE_EVERY = int(os.getenv("PARSE_EVERY"))
