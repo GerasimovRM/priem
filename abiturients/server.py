6aab5484bec0daac1122a87ff2362371eab6d1a9
Roman Gersaimov <romagrizly@gmail.com>
2022-08-09 01:28:50 +0500

    work version ege

--- abiturients/server.py
+++ abiturients/server.py
@@ -10,19 +10,23 @@ from common import group_names
 
 app = FastAPI(docs_url="/")
 
-
 exclude_data = {"ist": 63, "fop": 22, "pm": 38, "ivk": 23, "mie": 22, "mm": 37}
 delete_keys = ["Подтверждающий документ (целевого направления)", "Преимущ.право"]
 students_codes = {}
 
 
 def update_rating(abiturients, enumerate_with=1):
-    abiturients = [{k: v if v else "-" for k, v in abiturient.items() if k not in delete_keys} for abiturient in abiturients]
-    return list(map(lambda abit_data: {"Рейтинг в текущем списке": abit_data[0]} | abit_data[1], enumerate(abiturients, enumerate_with)))
+    abiturients = [{k: v if v else "-" for k, v in abiturient.items() if k not in delete_keys} for
+                   abiturient in abiturients]
+    return list(map(lambda abit_data: {"Рейтинг в текущем списке": abit_data[0]} | abit_data[1],
+                    enumerate(abiturients, enumerate_with)))
 
 
 def add_students_names(abiturients):
-    return list(map(lambda abit_data: dict(zip(["ФИО", "_____Номер_____", "Почта"], students_codes[abit_data["_id"]] if abit_data["_id"] in students_codes else ["-", "-", "-"])) | abit_data, abiturients))
+    return list(map(lambda abit_data: dict(zip(["ФИО", "_____Номер_____", "Почта"],
+                                               students_codes[abit_data["_id"]] if abit_data[
+                                                                                       "_id"] in students_codes else [
+                                                   "-", "-", "-"])) | abit_data, abiturients))
 
 
 @app.on_event("startup")
@@ -50,7 +54,8 @@ async def links():
 async def get_default():
     client = DbClient()
     abiturients = client["abiturients"]
-    return {group_name: update_rating(add_students_names(abiturients[group_name].find({}))) for group_name in group_names}
+    return {group_name: update_rating(add_students_names(abiturients[group_name].find({}))) for
+            group_name in group_names}
 
 
 @app.get("/get_original_docs_without_agreement")
@@ -58,7 +63,8 @@ async def get_original_docs_without_agreement():
     client = DbClient()
     query = {"Вид документа об образовании": "Оригинал", "Согласие на зачисление": None}
     abiturients = client["abiturients"]
-    return {group_name: update_rating(add_students_names(abiturients[group_name].find(query))) for group_name in group_names}
+    return {group_name: update_rating(add_students_names(abiturients[group_name].find(query))) for
+            group_name in group_names}
 
 
 @app.get("/get_agreement_without_original_docs")
@@ -66,15 +72,26 @@ async def get_agreement_without_original_docs():
     client = DbClient()
     query = {"Вид документа об образовании": "Копия", "Согласие на зачисление": "✓"}
     abiturients = client["abiturients"]
-    return {group_name: update_rating(add_students_names(abiturients[group_name].find(query))) for group_name in group_names}
+    return {group_name: update_rating(add_students_names(abiturients[group_name].find(query))) for
+            group_name in group_names}
 
 
 @app.get("/get_agreement_with_original_docs")
 async def get_agreement_with_original_docs():
     client = DbClient()
     abiturients = client["abiturients"]
-    query = {"Вид документа об образовании": "Оригинал", "Согласие на зачисление": "✓"}
-    return {group_name: update_rating(add_students_names(abiturients[group_name].find(query))) for group_name in group_names}
+    query = {"$or": [
+        {
+            "Вид документа об образовании": "Оригинал",
+            "Согласие на зачисление": "✓",
+            "Доставка документов": "Веб"
+        },
+        {
+            "Согласие на зачисление": "✓",
+            "Доставка документов": "Суперсервис"
+        }]}
+    return {group_name: update_rating(add_students_names(abiturients[group_name].find(query))) for
+            group_name in group_names}
 
 
 @app.get("/get_exclude_peoples")
@@ -82,9 +99,10 @@ async def get_exclude_peoples():
     client = DbClient()
     abiturients = client["abiturients"]
     query = {"Вид документа об образовании": "Оригинал", "Согласие на зачисление": "✓"}
-    return {group_name: update_rating(add_students_names(abiturients[group_name].find(query)[exclude_data[group_name]:]),
-                                      enumerate_with=exclude_data[group_name] + 1) for group_name in group_names}
+    return {group_name: update_rating(
+        add_students_names(abiturients[group_name].find(query)[exclude_data[group_name]:]),
+        enumerate_with=exclude_data[group_name] + 1) for group_name in group_names}
+
 
 if __name__ == "__main__":
-
     uvicorn.run("server:app", host="0.0.0.0", port=5000, log_level="info", reload=True)
---
+++ ege_vedomosti/generate_ege_ved.py
@@ -0,0 +1,153 @@
+import json
+from copy import copy
+import re
+
+import openpyxl
+from openpyxl import load_workbook, Workbook
+from openpyxl.cell import Cell, MergedCell
+from openpyxl.descriptors.excel import CellRange
+from openpyxl.utils import get_column_letter
+from openpyxl.worksheet.worksheet import Worksheet
+from openpyxl.worksheet.dimensions import DimensionHolder, ColumnDimension
+
+
+
+
+with open("generate_data.json", encoding="utf-8") as json_data:
+    generate_data = json.load(json_data)
+
+abiturients_data = {}
+data_xlsx = load_workbook("data.xlsx")
+sheet = data_xlsx.worksheets[0]
+next_row = None
+current_row = 1
+subjects = False
+for i, row in enumerate(sheet.rows, current_row):
+    if subjects:
+        subject_data = []
+        for j, cell in enumerate(row, 1):
+            if isinstance(cell, Cell) and cell.value is not None:
+                if cell.value == "Справка для личного дела абитуриента сформирована из ФИС ГИА и приема для образовательной организации:":
+                    subjects = False
+                    break
+                subject_data.append(cell.value)
+        if subjects and subject_data:
+            if "subjects" not in abiturients_data[current_abiturient]:
+                abiturients_data[current_abiturient]["subjects"] = []
+            abiturients_data[current_abiturient]["subjects"].append(subject_data)
+    else:
+        for j, cell in enumerate(row, 1):
+            if isinstance(cell, Cell) and cell.value is not None:
+                # print(cell, cell.value)
+                if cell.value == "о результатах единого государственного экзамена":
+                    current_abiturient = sheet.cell(i + 2, j).value
+                    if current_abiturient not in abiturients_data:
+                        abiturients_data[current_abiturient] = {}
+                elif "документ, удостоверяющий личность:" in cell.value:
+                    if "passport" not in abiturients_data[current_abiturient]:
+                        abiturients_data[current_abiturient]["passport"] = cell.value.split(":")[-1].strip()
+                elif cell.value == "Наименование предмета":
+                    subjects = True
+                    break
+print(abiturients_data)
+
+template = load_workbook("template.xlsx")
+ws_template = template.worksheets[0]
+
+result_xlsx = Workbook()
+result_xlsx.create_sheet("Лист1")
+ws_res = result_xlsx.worksheets[0]
+
+for idx, rd in ws_template.row_dimensions.items():
+    ws_res.row_dimensions[idx] = copy(rd)
+
+for idx, cd in ws_template.column_dimensions.items():
+     ws_res.column_dimensions[idx] = copy(cd)
+
+no_fill = openpyxl.styles.PatternFill(fill_type=None)
+side = openpyxl.styles.Side(border_style=None)
+no_border = openpyxl.styles.borders.Border(
+    left=side,
+    right=side,
+    top=side,
+    bottom=side,
+)
+
+# Loop through all cells in all worksheets
+for row in ws_res:
+    for cell in row:
+        # Apply colorless and borderless styles
+        cell.fill = no_fill
+        cell.border = no_border
+
+
+for current_abiturient in abiturients_data:
+    for i, row in enumerate(ws_template.rows, 1):
+        for j, col in enumerate(row, 1):
+            template_value = ws_template.cell(i, j).value
+            if template_value is not None:
+                if "<template: fio>" in template_value:
+                    template_value = template_value.replace("<template: fio>", current_abiturient)
+                elif "<template: pasport_data>" in template_value:
+                    template_value = template_value.replace("<template: pasport_data>", abiturients_data[current_abiturient]["passport"])
+                elif "<template: date>" in template_value:
+                    template_value = template_value.replace("<template: date>",
+                                                            generate_data["date"])
+                elif "<template: employee1_fio>" in template_value:
+                    template_value = template_value.replace("<template: employee1_fio>",
+                                                            generate_data["employee1_fio"])
+                elif "<template: employee1_position>" in template_value:
+                    template_value = template_value.replace("<template: employee1_position>",
+                                                            generate_data["employee1_position"])
+                elif "<template: employee2_fio>" in template_value:
+                    template_value = template_value.replace("<template: employee2_fio>",
+                                                            generate_data["employee2_fio"])
+                elif "<template: employee2_position>" in template_value:
+                    template_value = template_value.replace("<template: employee2_position>",
+                                                            generate_data["employee2_position"])
+
+            ws_res.cell(i, j).value = template_value
+            if ws_template.cell(i, j).has_style:
+                ws_res.cell(i, j).font = copy(ws_template.cell(i, j).font)
+                ws_res.cell(i, j).border = copy(ws_template.cell(i, j).border)
+                ws_res.cell(i, j).fill = copy(ws_template.cell(i, j).fill)
+                ws_res.cell(i, j).number_format = copy(ws_template.cell(i, j).number_format)
+                ws_res.cell(i, j).protection = copy(ws_template.cell(i, j).protection)
+                ws_res.cell(i, j).alignment = copy(ws_template.cell(i, j).alignment)
+
+    for elem in ws_template.merged_cells:
+        rows = list(elem.rows)[0]
+        row = rows[0][0]
+        cols = list(map(lambda x: x[1], rows))
+        ws_res.merge_cells(start_row=row, end_row=row, start_column=cols[0], end_column=cols[-1])
+        print(row, cols)
+        print("=" * 20)
+
+    for i, row in enumerate(ws_template.rows, 1):
+        for j, col in enumerate(row, 1):
+            template_value = ws_template.cell(i, j).value
+            if template_value is not None:
+                if "<template: exams_data>" in template_value:
+                    head_i = i - 1
+                    for ii in range(len(abiturients_data[current_abiturient]["subjects"])):
+                        ws_res.merge_cells(start_row=i + ii, end_row=i + ii, start_column=1, end_column=2)
+                        ws_res.merge_cells(start_row=i + ii, end_row=i + ii, start_column=5, end_column=6)
+                        ws_res.merge_cells(start_row=i + ii, end_row=i + ii, start_column=7, end_column=8)
+                        subject = abiturients_data[current_abiturient]["subjects"][ii]
+                        ws_res.cell(i + ii, 1, subject[0])
+                        ws_res.cell(i + ii, 3, subject[1])
+                        ws_res.cell(i + ii, 4, subject[2])
+                        ws_res.cell(i + ii, 5, subject[3])
+                        if len(subject) > 4:
+                            ws_res.cell(i + ii, 7, subject[4])
+                        for k in range(1, 11):
+                            if ws_template.cell(head_i, k).has_style:
+                                ws_res.cell(i + ii, k).font = copy(ws_template.cell(head_i, k).font)
+                                ws_res.cell(i + ii, k).border = copy(ws_template.cell(head_i, k).border)
+                                ws_res.cell(i + ii, k).fill = copy(ws_template.cell(head_i, k).fill)
+                                ws_res.cell(i + ii, k).number_format = copy(ws_template.cell(head_i, k).number_format)
+                                ws_res.cell(i + ii, k).protection = copy(ws_template.cell(head_i, k).protection)
+                                ws_res.cell(i + ii, k).alignment = copy(ws_template.cell(head_i, k).alignment)
+                    break
+
+result_xlsx.save("result.xlsx")
