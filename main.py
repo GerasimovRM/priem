22e5e0b419aeb3217b263c7b6a7c53518cca6e36
Roman Gersaimov <romagrizly@gmail.com>
2022-08-15 12:26:16 +0500

    magistr upload

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ import requests
 
 from from_site_to_archives import FromCiteToArchive
 
+parser = FromCiteToArchive("config.ini")
+
+"""
 # run abiturients.server
 abiturients_with_original_and_agreement = requests.get("http://127.0.0.1:5000/get_agreement_with_original_docs").json()
 # pprint(abiturients_with_original_and_agreement)
 
-parser = FromCiteToArchive("config.ini")
 zip_dir = parser.zip_dir
 for direction in abiturients_with_original_and_agreement:
     print(direction, "=" * 10)
@@ -19,12 +21,9 @@ for direction in abiturients_with_original_and_agreement:
         else:
             parser.find_person_and_load_data(abiturient["ФИО"], direction)
 parser.close_all_webdrivers()
+"""
 
 
-# with open("students.txt", encoding="utf-8") as students_file:
-#     for student_fio in map(str.strip, students_file.readlines()):
-#         parser.find_person_and_load_data(student_fio)
-# parser.close()
-# input("Нажмите для выхода любую клваишу")
+
 #
-# import from_archives_to_result_pdfs
+import from_archives_to_result_pdfs
